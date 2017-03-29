beaca3c3b1715d9f9b2f889a5d686c794d4e9ad1
ricky840 <ricky840@gmail.com>
2017-03-29 20:02:59 +0900

    Initial Commit

---
+++ FastCampusRickyManualLayoutTest/FastCampusRickyManualLayoutTest.h
@@ -0,0 +1,19 @@
+//
+//  FastCampusRickyManualLayoutTest.h
+//  FastCampusRickyManualLayoutTest
+//
+//  Created by Ricky on 3/29/17.
+//  Copyright © 2017 Ricky Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastCampusRickyManualLayoutTest.
+FOUNDATION_EXPORT double FastCampusRickyManualLayoutTestVersionNumber;
+
+//! Project version string for FastCampusRickyManualLayoutTest.
+FOUNDATION_EXPORT const unsigned char FastCampusRickyManualLayoutTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastCampusRickyManualLayoutTest/PublicHeader.h>
+
+
